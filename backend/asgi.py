d4d51e41ec48778632e0f6420da674bbf1a5ffa3
Waliimran <waliimran.dev@gmail.com>
2026-03-26 18:25:22 +0500

    fixed server crash

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,11 +1,13 @@
 import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from api.middleware import JWTAuthMiddleware
 from api.routing import websocket_urlpatterns
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
-
 
 application = ProtocolTypeRouter(
     {
